89224b745c4114a80ad1c9fb8efeaaf9f572037d
nova vita <42131370+josephc1417@users.noreply.github.com>
2023-09-02 04:00:41 -0400

    Added footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,44 +1,34 @@
-
+import { PageLink,SocialLinks } from '../data'
 
 export const Footer = () => {
   return (
     <div>
         
     <footer className="section footer">
-      <ul className="footer-links">
-        <li>
-          <a href="#home" className="footer-link">home</a>
-        </li>
-        <li>
-          <a href="#about" className="footer-link">about</a>
-        </li>
-        <li>
-          <a href="#services" className="footer-link">services</a>
-        </li>
-        <li>
-          <a href="#featured" className="footer-link">featured</a>
-        </li>
-      </ul>
+    <ul className="footer-links">
+          {PageLink.map((link) => {
+            return(
+          <li key={link.id}>
+            <a href={link.href} className="footer-link">{link.text}</a>
+          </li>
+            );
+          })}
+        </ul>
       <ul className="footer-icons">
-        <li>
-          <a href="https://www.twitter.com" target="_blank" className="footer-icon"
-            ><i className="fab fa-facebook"></i
-          ></a>
-        </li>
-        <li>
-          <a href="https://www.twitter.com" target="_blank" className="footer-icon"
-            ><i className="fab fa-twitter"></i
-          ></a>
-        </li>
-        <li>
-          <a href="https://www.twitter.com" target="_blank" className="footer-icon"
-            ><i className="fab fa-squarespace"></i
-          ></a>
-        </li>
+      {SocialLinks.map((slink) => {
+            const {id,href,target,i} = slink
+           return(
+           <li key={id}>
+            <a href={href} target={target} rel='noreferrer' className="nav-icon"
+              ><i className={i}></i
+            ></a>
+          </li>
+           )
+          })}
       </ul>
       <p className="copyright">
         copyright &copy; Backroads travel tours company
-        <span id="date"></span> all rights reserved
+        <span id="date">{new Date().getFullYear()}</span> all rights reserved
       </p>
     </footer>
     </div>
